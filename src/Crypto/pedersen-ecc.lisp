48a851b1f4509e6afd8a21271ec2cea2e0f858fb
David McClain <dbm@refined-audiometrics.com>
2018-02-20 12:58:58 -0700

    bring imports up to snuff

--- src/Crypto/pedersen-ecc.lisp
+++ src/Crypto/pedersen-ecc.lisp
@@ -30,26 +30,31 @@ THE SOFTWARE.
 (user::asdf :core-crypto)
 
 (defpackage :pedersen-ecc
-  (:use :common-lisp)
-  (:import-from :ecc-crypto-b571
+  (:use :common-lisp :crypto-mod-math)
+  (:import-from :edwards-ecc
    :ed-mul
    :ed-add
    :ed-sub
    :ed-div
+   :ed-negate
    :ed-affine
    :*edcurve*
    :*ed-r*
+   :*ed-q*
    :*ed-gen*
    :ed-curve-name
    :random-between
    :ed-pt=
    :ed-compress-pt
    :ed-decompress-pt
-   :sha3-buffers
    :convert-bytes-to-int
    :with-ed-curve
-   :*curve-e521*
+   :ed-nth-pt
+   :ed-random-pair
    )
+  (:import-from :ecc-crypto-b571
+   :convert-int-to-nbytesv
+   :convert-bytes-to-int)
   (:export
    ))
 
@@ -57,8 +62,7 @@ THE SOFTWARE.
 
 (defun rand-val ()
   ;; random value in Z_r
-  (with-ed-curve *curve-e521*
-    (random-between 1 *ed-r*)))
+  (random-between (ash *ed-r* -1) *ed-r*))
 
 (defun hashem-int (&rest args)
   (convert-bytes-to-int
@@ -75,20 +79,19 @@ THE SOFTWARE.
 (defun commit-vector (a-vec g-vec h-gen blinding-s)
   ;; h-gen = (pf-expt *pf-rg* secret-a)
   ;; keep secrets: secret-a, blinding-s
-  (with-ed-curve *curve-e521*
-    (make-commitment
-     :curve  (ed-curve-name *edcurve*)
-     :gens   (mapcar 'ed-compress-pt g-vec)
-     :hgen   (ed-compress-pt h-gen)
-     :com    (ed-compress-pt
-              ;; s*h + (a . g), for a, g vectors
-              (ed-add
-               ;; shield dot prod with blinding
-               (ed-mul h-gen blinding-s)
-               ;; compute g^(alpha .dot. a)
-               (reduce 'ed-add
-                       (mapcar 'ed-mul g-vec a-vec))))
-     )))
+  (make-commitment
+   :curve  (ed-curve-name *edcurve*)
+   :gens   (mapcar 'ed-compress-pt g-vec)
+   :hgen   (ed-compress-pt h-gen)
+   :com    (ed-compress-pt
+            ;; s*h + (a . g), for a, g vectors
+            (ed-add
+             ;; shield dot prod with blinding
+             (ed-mul h-gen blinding-s)
+             ;; compute g^(alpha .dot. a)
+             (reduce 'ed-add
+                     (mapcar 'ed-mul g-vec a-vec))))
+   ))
 
 ;; ---------------------------------------------------------------
 ;; COMMIT - For a vector of values a-vec, create a Pedersen Commitment
@@ -101,39 +104,93 @@ THE SOFTWARE.
 (defun commit (a-vec)
   ;; send com portion, keep secret-a & blinding-s secret for now
   ;; when ready to reveal the commited values, send a-vec and blinding-s
-  (with-ed-curve *curve-e521*
-    (let* ((secret-a   (rand-val)) ;; never shared
-           (blinding-s (apply 'hashem-int secret-a a-vec)) ;; (rand-val))
-           (g-vec      (make-gens (length a-vec)))
-           (h-gen      (ed-mul *ed-gen* secret-a))
-           (com        (commit-vector a-vec g-vec h-gen blinding-s)))
-      (list :com com ;; public share
-            :secret-a secret-a  ;; keep secret
-            :blinding-s blinding-s)))) ;; keep secret
+  (let* ((secret-a   (rand-val)) ;; never shared
+         (blinding-s (apply 'hashem-int secret-a a-vec)) ;; (rand-val))
+         (g-vec      (make-gens (length a-vec)))
+         (h-gen      (ed-mul *ed-gen* secret-a))
+         (com        (commit-vector a-vec g-vec h-gen blinding-s)))
+    (list :com com ;; public share
+          :secret-a secret-a  ;; keep secret
+          :blinding-s blinding-s))) ;; keep secret
   
   ;; -------------------------------------------------------------------
 
 (defun make-gens (nel)
   ;; form a random blinding vector of generators g^alpha_i
   ;; ensure no duplicates (very small chance of that)
-  (with-ed-curve *curve-e521*
-    (um:nlet-tail iter ((nel nel)
-                        (ans nil))
-      (if (plusp nel)
-          (let ((g-alpha (ed-mul *ed-gen* (rand-val))))
-            (if (member g-alpha ans :test 'ed-pt=)
-                (iter nel ans)
-              (iter (1- nel) (cons g-alpha ans))))
-        ans))))
+  (um:nlet-tail iter ((nel nel)
+                      (ans nil))
+    (if (plusp nel)
+        (let ((g-alpha (ed-mul *ed-gen* (rand-val))))
+          (if (member g-alpha ans :test 'ed-pt=)
+              (iter nel ans)
+            (iter (1- nel) (cons g-alpha ans))))
+      ans)))
 
 (defun make-coffs (nel)
   ;; choose some random values for test a-vec
   (loop repeat nel collect
         (rand-val)))
 
+;; ------------------------------------------------------------------
+
+(defun hash-length-bits ()
+  (1+ (integer-length *ed-q*)))
+
+(defun select-sha3-hash ()
+  (let ((nb  (hash-length-bits)))
+    (cond ((> nb 384) :sha3)
+          ((> nb 256) :sha3/384)
+          (t          :sha3/256)
+          )))
+
+(defun sha3-buffers (&rest bufs)
+  ;; assumes all buffers are UB8
+  (let ((dig  (ironclad:make-digest (select-sha3-hash))))
+    (dolist (buf bufs)
+      (ironclad:update-digest dig buf))
+    (ironclad:produce-digest dig)))
+
+(defun convert-pt-to-v (pt)
+  (let ((nb (ceiling (1+ (integer-length *ed-q*)) 8)))
+    (convert-int-to-nbytesv (ed-compress-pt pt) nb)))
+
+(defun hash-pt-pt (pt1 pt2)
+  (let ((v1 (convert-pt-to-v pt1))
+        (v2 (convert-pt-to-v pt2)))
+    (convert-bytes-to-int (sha3-buffers v1 v2))))
+
+(defun hash-pts (&rest pts)
+  (convert-bytes-to-int
+   (apply 'sha3-buffers (mapcar 'convert-pt-to-v pts))))
+
+;; --------------------------------------------------------------------
+
 #|
 ;; try it out... 
-(let* ((nel  5)
+(let* ((nel  4)
        (av   (make-coffs nel)))
   (inspect (commit av)))
  |#
+#|
+;; -----------------------------------------------------------------------
+;; ZKP to prove N >= 0, select the 4 integers from Lagrange 4-square,
+;; whose sum of squares equals our N. Hence, sum >= 0.
+;;
+;; Make a commitment on these 4 numbers:
+;;    (n1 n2 n3 n4) -> Sum(n_i * p_i, i = 1..4) for p_i prime
+;;
+;; If the p_i are all coprime and > largest possible n_i then this sum is unique
+;; per ordering of n_i. Random blinding value v, secret key s,
+;; commitment (msg = Sum*P - V, c = H(V,msg),  r = v - c*s)
+;;   so R = r*G = v*G - c*s*G = v*G - c*P = V - c*P, or V = r*G + c*P
+;;      check H(V,msg) = c
+;;      Sum*P = msg + V
+
+(inspect
+ (sort
+  (loop for ix from 0 below 11 nconc
+        (loop for iy from 0 below 11 collect (+ (* ix 11) (* iy 13))))
+  '<))
+|#
+    
