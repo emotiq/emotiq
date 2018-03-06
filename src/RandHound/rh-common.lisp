5480ea99a1882a57eb3a2437f93cf455bd5fd1b6
David McClain <dbm@emotiq.ch>
2018-03-06 10:56:51 -0700

    Fix typo

--- src/RandHound/rh-common.lisp
+++ src/RandHound/rh-common.lisp
@@ -26,7 +26,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 |#
 
-(in-package :randhound/ccommon)
+(in-package :randhound/common)
 
 ;; ---------------------------------------------------------------
 ;; Nodes Directory
