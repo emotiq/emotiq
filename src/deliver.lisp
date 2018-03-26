a3a904c9da14a0d889998aca473c07709e35eef8
Paul Tarvydas <paul.tarvydas@gmail.com>
2018-03-26 16:22:32 -0400

    first cut deliver.lisp, includes call to init-pairings (removed from pbc-cffi.lisp)

---
+++ src/deliver.lisp
@@ -0,0 +1,16 @@
+(in-package "CL-USER")
+(load-all-patches)
+(load "~/quicklisp/setup.lisp")
+(ql:quickload :emotiq)
+(ql:quickload :crypto-pairings)
+(ql:quickload :core-crypto)
+
+(defun main ()
+  (pbc-interface:init-pairing)
+  (pbc:make-key-pair :dave)
+  (let ((signed (pbc:sign-message :hello)))
+    (if (pbc:check-message signed)
+        (format *standard-output* "~%OK~%")
+      (format *standard-output* "~%NOT OK~%"))))
+
+(deliver 'main "emotiq" 0)
