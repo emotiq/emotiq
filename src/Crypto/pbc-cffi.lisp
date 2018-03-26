6c26b62a260a246601536db8b19aeedeb36d82ec
Paul Tarvydas <paul.tarvydas@gmail.com>
2018-03-26 09:17:54 -0400

    clean build and run, having moved files from /usr/local/lib and /usr/local/include

--- src/Crypto/pbc-cffi.lisp
+++ src/Crypto/pbc-cffi.lisp
@@ -96,9 +96,14 @@ THE SOFTWARE.
 
 ;; -----------------------------------------------------------------------
 
+;; (cffi:define-foreign-library libpbc
+;;   (:darwin "/usr/local/lib64/libLispPBCIntf.dylib")
+;;   (:linux  "/usr/local/lib/libLispPBCIntf.so")
+;;   (t (:default "libLispPBCIntf"))
+;;   )
 (cffi:define-foreign-library libpbc
-  (:darwin "/usr/local/lib64/libLispPBCIntf.dylib")
-  (:linux  "/usr/local/lib/libLispPBCIntf.so")
+  (:darwin "/home/tarvydas/work/emotiq/lib/libLispPBCIntf.dylib")
+  (:linux  "/home/tarvydas/work/emotiq/lib/libLispPBCIntf.so")
   (t (:default "libLispPBCIntf"))
   )
 (cffi:use-foreign-library libpbc)
