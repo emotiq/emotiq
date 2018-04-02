70a54ce11a3c254d0d3629bae750e6d9aa13fde1
Paul Tarvydas <paul.tarvydas@gmail.com>
2018-04-02 15:27:32 -0400

    removed simple-test*

--- src/simple-test.lisp
+++
@@ -1,9 +0,0 @@
-(in-package "EMOTIQ")
-
-(defun simple-test ()
-  (pbc:make-key-pair :dave)
-  (let ((signed (pbc:sign-message :hello)))
-    (if (pbc:check-message signed)
-        (format *standard-output* "~%OK~%")
-      (format *standard-output* "~%NOT OK~%"))))
-
