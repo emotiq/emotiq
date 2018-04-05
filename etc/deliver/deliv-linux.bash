68e253daf424a7a948b5dac216c81306f02b0281
Paul Tarvydas <tarvydas@pauls-mac-mini-2.local>
2018-04-04 23:24:26 -0400

    wip: towards mac binary

--- etc/deliver/deliv-linux.bash
+++ etc/deliver/deliv-linux.bash
@@ -4,6 +4,6 @@ DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 LWPRO=${LWPRO:-/usr/local/lib64/LispWorks/lispworks-7-1-0-amd64-linux}
 
-exec "${LWPRO}" -build "${DIR}/../../src/deliver.lisp"
+exec "${LWPRO}" -build "${DIR}/deliver/deliver.lisp"
 
 
