a9ab390f28723c93ae3f19be5848a84f8417bcbc
Paul Tarvydas <paul@emotiq.ch>
2018-04-05 01:15:19 -0400

    unsaved buffer for deliver.lisp

--- etc/deliver/deliver.lisp
+++ etc/deliver/deliver.lisp
@@ -1,7 +1,7 @@
 (in-package "CL-USER")
 (load-all-patches)
 (load "~/quicklisp/setup.lisp")
-(ql:quickload :emotiq)
+
 (ql:quickload :emotiq/startup)
 
 (deliver 'emotiq:production-start "emotiq" 0 :multiprocessing t :console t)
