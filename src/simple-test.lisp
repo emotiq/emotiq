897361e9b93b862ca02bac166b337d4e0dc7b757
Paul Tarvydas <tarvydas@pauls-mac-mini-2.local>
2018-04-04 22:59:20 -0400

    startup finished - (ql:quickload :emotiq/startup) (emotiq:dev-start)  [also dumps memory usage]

--- src/simple-test.lisp
+++
@@ -1,9 +0,0 @@
-(in-package "CL-USER")
-
-(defun main ()
-  (pbc:make-key-pair :dave)
-  (let ((signed (pbc:sign-message :hello)))
-    (if (pbc:check-message signed)
-        (format *standard-output* "~%OK~%")
-      (format *standard-output* "~%NOT OK~%"))))
-
