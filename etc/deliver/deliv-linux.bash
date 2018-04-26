e3a22d123bfe58e419a721722b55fd4ada118c1e
Eugene Chupriyanov <evgeniy.chupriyanov@veon.com>
2018-04-26 15:54:25 +0700

    [#156925103] Fixing path to LispWorks binary in Travis CI

--- etc/deliver/deliv-linux.bash
+++ etc/deliver/deliv-linux.bash
@@ -2,8 +2,7 @@
 
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-LWPRO=${LWPRO:-/usr/local/lib64/LispWorks/lispworks-7-1-0-amd64-linux}
+# LWPRO=${LWPRO:-/usr/local/lib64/LispWorks/lispworks-7-1-0-amd64-linux}
+LWPRO=${LWPRO:-~/.roswell/impls/x86-64/linux/lispworks-bin/7.1.0.0/lwpro}
 
 "${LWPRO}" -build "${DIR}/deliver.lisp"
-
-
