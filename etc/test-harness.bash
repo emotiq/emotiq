e6af342b025e94cb169c9c698a8e0cb5150b4246
Mark <evenson.not.org@gmail.com>
2018-04-28 10:42:18 +0200

    [#157169441] Aesthetic adjustment to user messages
    Note inability to directly invoke LispWorks as a bug.

--- etc/test-harness.bash
+++ etc/test-harness.bash
@@ -11,34 +11,41 @@
 #
 # For example
 #
-#   lisp=ccl systems=:gossip-test bash test-harness.bash
+#   lisp=ccl systems=":gossip-test :cosi-bls" bash test-harness.bash
 #
-# would invoke the test harness using `ccl` on the `gossip-test`
-# system.
+# would invoke the test harness using `ccl` on the `gossip-test` and
+# `cosi-bls` systems.
+#
+# BUGS
+#
+#  LispWorks cannot directly be used due to the use of single dash
+#  command line options (i.e. `-eval` instead of `--eval`).  Instead,
+#  an invocation wrapper like Roswell must be used to invoke
+#  LispWorks.
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # The command to run a Lisp test form:
 lisp=${lisp:-'ros'}
 
-# whitespace list of systems to test
+# The default whitespace list of systems to test
 systems=${systems:-":emotiq/blockchain
                     :crypto-pairings/t
                     :core-crypto"}
 
 echo Test harness invoked using implementation:
-echo $(${lisp} --eval '(format t "~&~a~&~a~&" (lisp-implementation-type)(lisp-implementation-version))(uiop:quit 0)')
+echo $(${lisp} --eval '(format t "~&~a ~a~&" (lisp-implementation-type)(lisp-implementation-version))(uiop:quit 0)')
 
 for system in ${systems}; do
     rm -rf ~/.cache/common-lisp/
-    echo "==>Invoking ASDF:TEST-SYSTEM on ${system}..."
+    echo "==> ASDF:TEST-SYSTEM invoked on ${system}..."
     ${lisp} \
         --load ${DIR}/test-harness.lisp \
         --eval "(test-harness:test-system ${system})"
     status=$?
     if [[ $status -ne 0 ]] ; then
-        echo "ASDF:TEST-SYSTEM failed for ${system}"
+        echo "!== ASDF:TEST-SYSTEM failed for ${system}"
         exit $status
     fi
-    echo "<==ASDF:TEST-SYSTEM on ${system} succeeded."
+    echo "<== ASDF:TEST-SYSTEM on ${system} succeeded."
 done
                                       
