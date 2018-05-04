f79c56be11eaf80fcfbcff8960a63d53173969d2
Eugene Chupriyanov <evgeniy.chupriyanov@veon.com>
2018-05-04 13:09:19 +0300

    Switched to our ownn binaries for SBCL

--- ci/install-sbcl.sh
+++ ci/install-sbcl.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
 install_sbcl_brew() {
-  brew update && brew install sbcl
+  wget -O /tmp/sbcl.bz2 https://github.com/emotiq/sbcl-binaries/releases/download/1.4.7/sbcl-1.4.7-x86-64-darwin-binary.tar.bz2
+  (cd /tmp && \
+    tar xfj sbcl.bz2 && \
+    cd sbcl-1.4.7-x86-64-darwin && \
+    ./install.sh
+  )
 }
 
 install_sbcl_linux() {
   sudo apt-get update && apt-get install -y \
     build-essential \
     wget
-  wget -O /tmp/sbcl.bz2 http://prdownloads.sourceforge.net/sbcl/sbcl-1.4.7-x86-64-linux-binary.tar.bz2
+  wget -O /tmp/sbcl.bz2 https://github.com/emotiq/sbcl-binaries/releases/download/1.4.7/sbcl-1.4.7-x86-64-linux-binary.tar.bz2
   (cd /tmp && \
-    tar xvfj sbcl.bz2 && \
+    tar xfj sbcl.bz2 && \
     cd sbcl-1.4.7-x86-64-linux && \
     ./install.sh
   )
