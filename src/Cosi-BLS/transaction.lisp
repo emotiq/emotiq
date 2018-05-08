5c75f94680c4d93d6f8b3bb533bb24028edd8a7b
David McClain <dbm@emotiq.ch>
2018-05-08 10:13:34 -0700

    add get-txin-amount to transactions

--- src/Cosi-BLS/transaction.lisp
+++ src/Cosi-BLS/transaction.lisp
@@ -233,7 +233,15 @@ correction factor gamadj on curve H for the overall transaction."
 ;; ---------------------------------------------------------------------
 ;; Recover spend info
 
-(defun decrypt-txin-info  (txin skey)
+(defmethod get-txin-amount ((txin uncloaked-txin))
+  (txin-amt txin))
+
+(defmethod get-txin-amount ((txin txin))
+  (decrypt-txin-info txin))
+
+
+
+(defmethod decrypt-txin-info  ((txin txin) skey)
   (pbc:ibe-decrypt (txin-encr txin) skey))
 
 (defmethod find-txin-for-pkey-hash (pkey (trn transaction))
