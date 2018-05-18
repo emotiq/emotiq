0cca42032d41ca8475244ec3a7b28ca2af063230
Paul Tarvydas <paul.tarvydas@gmail.com>
2018-05-18 18:00:57 -0400

    uncloaked/cloaked transactions ; elections

--- src/handler.lisp
+++ src/handler.lisp
@@ -6,7 +6,6 @@
   "override make-node-dispatcher in cosi-handlers.lisp with our own dispatcher which delegates unknown
    messages to cosi-handlers.lisp"
   (ac:make-actor
-   ;; one of these closures is stored in the SELF slot of every node
    (lambda (&rest msg)
      (apply 'emotiq/sim::node-dispatcher node msg))))
 
@@ -15,4 +14,31 @@
 (in-package :emotiq/sim)
 
 (defun node-dispatcher (node &rest msg)
-  (apply 'cosi-simgen::node-dispatcher node msg))
+
+  (if (and (= 2 (length msg))
+           (eq :hold-an-election (first msg))
+           (numberp (second msg)))
+      (let ((n (second msg)))
+        (ac:pr (format nil "got :hold-an-election ~A" n))
+        (let ((winner (emotiq/elections:hold-election n)))
+          (let ((me (eq winner node)))
+            (ac:pr (format nil "~%election results(~A) ~A~%" n (if me " *ME* " " not me ")))
+            (ac:pr (format nil "~%winner ~A me=~A~%" winner node))
+            (cosi-simgen::send node :make-block))))
+    
+    ;; else
+    (if (and (= 1 (length msg))
+             (eq :make-block (first msg)))
+        (when (cosi-simgen::node-blockchain node) ;; don't publish empty blocks
+          (cosi-simgen::leader-exec node cosi-simgen::*cosi-prepare-timeout* cosi-simgen::*cosi-commit-timeout*))
+      
+      (if (and (= 1 (length msg))
+               (eq :block-finished (first msg))) ;; sent by tail end of cosi-handlers/leader-exec
+          (progn
+            (emotiq/elections::kill-beacon) ;; for simulator
+            (force-epoch-end)
+            (ac:pr "Block committed to blockchain")
+            (ac:pr (format nil "Block signatures = ~D" (logcount (cosi/proofs:block-signature-bitmap new-block)))))
+        
+        ;; else delegate message to cosi-simgen::node-dispatcher
+        (apply 'cosi-simgen::node-dispatcher node msg)))))
