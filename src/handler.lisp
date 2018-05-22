72bcdd334a55bbd293095386bc61e2813a5dbd80
Paul Tarvydas <paul.tarvydas@gmail.com>
2018-05-22 19:22:19 -0400

    more debug messages

--- src/handler.lisp
+++ src/handler.lisp
@@ -1,14 +1,17 @@
 (in-package :emotiq/sim)
 
 (defmethod cosi-simgen:node-dispatcher ((msg-sym (eql :hold-an-election)) &key n)
-  (increment-election-number)
-  (ac:pr (format nil "got :hold-an-election ~A ~A" n (election-number-or-string)))
   (let* ((node   (cosi-simgen:current-node))
          (winner (emotiq/elections:hold-election n)))
+    (ac:pr (format nil "got :hold-an-election ~A #~A ~A in ~A (~A)" n (election-number-or-string)
+                   (cosi-simgen::node-ip node)
+                   (cosi-simgen::node-real-ip node)
+                   (cosi-simgen::short-id node)))
     (let ((me (eq winner node)))
       (ac:pr (format nil "election results(~A) ~A" n (if me " *ME* " " not me ")))
       (ac:pr (format nil "winner ~A me=~A" winner node))
       (when me
+        (increment-election-number)
         (cosi-simgen:send node :make-block)))))
 
 (defmethod cosi-simgen:node-dispatcher ((msg-sym (eql :make-block)) &key)
