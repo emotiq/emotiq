82160b0d9c5cc4d13780dfff452377564ca5cd43
Paul Tarvydas <paul.tarvydas@gmail.com>
2018-05-22 11:08:22 -0400

    don't stop beacon

--- src/handler.lisp
+++ src/handler.lisp
@@ -1,17 +1,3 @@
-;; create a hook to node-dispatcher from cosi-handlers.lisp
-
-(in-package :cosi-simgen)
-
-#|
-(defun make-node-dispatcher (node)
-  "override make-node-dispatcher in cosi-handlers.lisp with our own dispatcher which delegates unknown
-   messages to cosi-handlers.lisp"
-  (ac:make-actor
-   (lambda (&rest msg)
-     (apply 'emotiq/sim::node-dispatcher node msg))))
-|#
-
-
 (in-package :emotiq/sim)
 
 
