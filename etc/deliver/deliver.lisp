37dfb6bc398fd3653962b74de05cd9a8122b4806
Paul Tarvydas <paul.tarvydas@gmail.com>
2018-05-25 12:12:02 -0400

    added :delivery feature

--- etc/deliver/deliver.lisp
+++ etc/deliver/deliver.lisp
@@ -3,10 +3,8 @@
 ;; QuickLisp should be properly configured by now
 (load "~/quicklisp/setup.lisp")
 
-(ql:quickload :emotiq/startup)
-
 (push :delivery *features*)
-
-(format t "~&features: ~A~&" *features*)
+(format *standard-output* "~&features: ~A~&" *features*)
+(ql:quickload :emotiq/startup)
 
 (deliver 'emotiq:start "emotiq" 0 :multiprocessing t :console t)
--- src/Actors/actors-startup.lisp
+++ src/Actors/actors-startup.lisp
@@ -226,13 +226,15 @@ THE SOFTWARE.
    :openmcl
    (and :lispworks (not :com.ral) (not :delivery)) )
 (eval-when (:load-toplevel :execute)
-  (install-actor-system))
+	   (format *standard-output* "~&IN EVAL-WHEN SECTION~&")
+	   (install-actor-system))
 
 #+(or
    (and :lispworks :com.ral)
-   (and :lispworks :delivery))
+   (and :lispworks :DELIVERY))
 (let ((lw:*handle-existing-action-in-action-list* '(:warn :skip)))
-  
+  (eval-when (:load-toplevel :execute)
+	     (format *standard-output* "~&IN DELIVERY SECTION~&"))
   (lw:define-action "Initialize LispWorks Tools"
                     "Start up Functional Actors"
                     'install-actor-system
