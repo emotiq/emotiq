5310baa3dab495daef42b4945a9e98498e36ed55
Eugene Chupriyanov <tchu@tchu.ru>
2018-05-29 12:32:46 +0300

    Added tests for binary build

--- etc/deliver/deliv-macos.bash
+++ etc/deliver/deliv-macos.bash
@@ -4,6 +4,7 @@
 # basically, this script calls LWM with two arguments '-build' and 'deliver.lisp'
 # Change the pathname appropriately for 64-bit LWM
 
+KIND=${1:-production}
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # LWPRO_MACOS_32=${LWPRO_MACOS_32:-/Applications/LispWork\ 7.0\ \(32-bit\)/LispWorks\ \(32-bit\).app/Contents/MacOS/lispworks-7-0-0-x86-darwin}
@@ -11,4 +12,4 @@ LWPRO_MACOS_64=${LWPRO_MACOS_64:-/Applications/LispWorks\ 7.1\ \(64-bit\)/LispWo
 
 LWPRO=${LWPRO_MACOS_64}
 
-"${LWPRO}" -build "${DIR}/deliver.lisp"
+"${LWPRO}" -build "${DIR}/${KIND}.lisp"
