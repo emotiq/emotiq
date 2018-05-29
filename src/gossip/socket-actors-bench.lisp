86a0517e8664a3576b98754d30f2d6a0d2686709
Luke Gorrie <luke@snabb.co>
2018-05-29 14:02:57 +0000

    Revise socket-actors-bench.lisp
    Add :THREAD wait mode that runs one thread per server socket (but
    still one for all clients.)
    
    Fix bug where :USOCKET wait method was not really tested.
    
    Rename parameter N to CONNECTIONS.
    
    Rename CSV column 'rate' to 'messages' to match the actual meaning.

--- src/gossip/socket-actors-bench.lisp
+++ src/gossip/socket-actors-bench.lisp
@@ -6,13 +6,13 @@
 
 (in-package :gossip)
 
-(defun benchmark-sendrecv-tcp-1 (&key (n 1) (seconds 1) (msgsize 100)
+(defun benchmark-sendrecv-tcp-1 (&key (connections 1) (seconds 1) (msgsize 100)
                                    (chatty 1.0)
                                    (encode :raw)
                                    (wait :nowait)
                                    (address "localhost"))
   "Run a benchmark of N client-server pairs sending unidirectional messages.
-   N is the number of client-server socket pairs to establish.
+   CONNECTIONS is the number of client-server socket pairs to establish.
    SECONDS is the duration of the benchmark.
    CHATTY is the chance of a socket being active on each iteration (0.0 - 1.0).
    ENCODE is :RAW or :SERIALIZE for how the message is encoded.
@@ -29,37 +29,59 @@
          (port (usocket:get-local-port listener))
          (clients '())
          (servers '())
+         (processes '()) ; processes that need to be shut down
          (messages 0))
     ;; Connnect client-server socket pairs.
-    (loop repeat n
+    (loop repeat connections
        do (push (usocket:socket-connect address port :nodelay :if-supported) clients)
        do (push (usocket:socket-accept listener) servers))
     ;; Helper functions
     (labels ((send ()
                (dolist (client clients)
-                 (when (< (random 1.0) chatty)
-                   (ecase encode
-                     (:raw
-                      (write-sequence msg (usocket:socket-stream client)))
-                     (:serialize
-                      (loenc:serialize msg (usocket:socket-stream client))))
-                   (finish-output (usocket:socket-stream client)))))
+                 (send-socket client)))
+             (send-socket (client)
+               (ecase encode
+                 (:raw
+                  (write-sequence msg (usocket:socket-stream client)))
+                 (:serialize
+                  (loenc:serialize msg (usocket:socket-stream client))))
+               (finish-output (usocket:socket-stream client)))
+             (serve-socket (server)
+               (unless (eq wait :thread)
+                 (ecase encode
+                   (:raw
+                    (read-sequence msg (usocket:socket-stream server)))
+                   (:serialize
+                    (loenc:deserialize (usocket:socket-stream server))))
+                 ;; Message received - Score a point.
+                 (incf messages)))
              (recv ()
-               (dolist (server (ecase wait
-                                 (:nowait servers)
-                                 (:usocket (usocket:wait-for-input servers))))
-                 (loop while (listen (usocket:socket-stream server))
-                      do (ecase encode
-                           (:raw
-                            (read-sequence msg (usocket:socket-stream server)))
-                           (:serialize
-                            (loenc:deserialize (usocket:socket-stream server))))
-                      ;; Message received - Score a point.
-                      (incf messages))))
+               (unless (eq wait :thread)
+                 (dolist (server (ecase wait
+                                   (:nowait servers)
+                                   (:usocket (usocket:wait-for-input servers))))
+                   (loop while (listen (usocket:socket-stream server))
+                      do (serve-socket server)))))
              (shutdown ()
                (usocket:socket-close listener)
+               (mapc #'mpcompat:process-kill processes) ; before closing sockets
                (mapc #'usocket:socket-close clients)
-               (mapc #'usocket:socket-close servers)))
+               (mapc #'usocket:socket-close servers))
+             (start-processes ()
+               (when (eq wait :thread)
+                 (dolist (server servers)
+                   (push (mpcompat:process-run-function
+                          "tcp benchmark server"
+                          nil
+                          (lambda () (loop do (serve-socket server))))
+                         processes))
+                 (dolist (client clients)
+                   (push (mpcompat:process-run-function
+                          "tcp benchmark client"
+                          nil
+                          (lambda () (loop do (send-socket client))))
+                         processes)))))
+      (start-processes)
       (unwind-protect 
            (loop with deadline = (+ (usec:get-time-usec) (* seconds 1000000))
               until (> (usec:get-time-usec) deadline)
@@ -69,7 +91,7 @@
         (shutdown)))))
 
 (defun suite-sendrecv-tcp-1 (&key
-                               (n '(1 10 25 50 100 200))
+                               (connections '(1 10 25 50 100 200))
                                (msgsize '(500 1000 1500 2000))
                                (chatty '(1.0))
                                (seconds '(1.0))
@@ -93,24 +115,24 @@
                      (make-broadcast-stream output-file *standard-output*)
                      *standard-output*)))
     ;; Populate TESTS with all permutations of variables.
-    (dolist (n n)
+    (dolist (n connections)
       (dolist (msgsize msgsize)
         (dolist (poll poll)
           (dolist (encode encode)
             (dolist (chatty chatty)
               (dolist (seconds seconds)
-                (push (list :n (max n 1) :msgsize msgsize :poll poll :encode encode :chatty chatty :seconds seconds) tests)))))))
+                (push (list :connections (max n 1) :msgsize msgsize :poll poll :encode encode :chatty chatty :seconds seconds) tests)))))))
     ;; Write prelude
     (format t "~&Running ~d tests~%" (length tests))
-    (format output "~&id,rate,n,msgsize,duration,poll,encode,chatty~%")
+    (format output "~&id,messages,connections,msgsize,duration,poll,encode,chatty~%")
     ;; Execute tests in a randomized order.
     (unwind-protect
          (dolist (test (alexandria:shuffle tests))
-           (destructuring-bind (&key n msgsize poll encode chatty seconds) test
+           (destructuring-bind (&key connections msgsize poll encode chatty seconds) test
              (format output "~&~d,~d,~d,~d,~d,~a,~a,~f~%"
                      (incf id)
-                     (benchmark-sendrecv-tcp-1 :n n :msgsize msgsize :seconds seconds)
-                     n msgsize seconds poll encode chatty)
+                     (benchmark-sendrecv-tcp-1 :connections connections :msgsize msgsize :seconds seconds :wait poll)
+                     connections msgsize seconds poll encode chatty)
              (finish-output output)))
       (unless (null output-file) (close output-file)))
     (format t "~&Benchmark suite completed.~%")))
