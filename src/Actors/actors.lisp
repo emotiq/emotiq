4334b5459fb8aca1bc8ee73cecb046bc52d2201e
Paul Tarvydas <paultarvydas@gmail.com>
2018-06-03 12:54:31 -0400

    nbr execs is 4 as per comment

--- src/Actors/actors.lisp
+++ src/Actors/actors.lisp
@@ -621,7 +621,7 @@ THE SOFTWARE.
 (defvar *executive-counter*  0)   ;; just a serial number on Executive threads
 (defvar *heartbeat-interval* 1)   ;; how often the watchdog should check for system stall
 (defvar *maximum-age*        3)   ;; how long before watchdog should bark
-(defvar *nbr-execs*          8)   ;; should match the number of CPU Cores but never less than 4
+(defvar *nbr-execs*          4)   ;; should match the number of CPU Cores but never less than 4
 
 ;; ----------------------------------------------------------------
 ;; Ready Queue
