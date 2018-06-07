9abb3f85a8a17ab578e41c716635ea8e84db42c1
Shannon Spires <ssos@bearlanding.com>
2018-06-07 10:56:04 -0600

    Shouldn't do ping-others here.

--- src/node.lisp
+++ src/node.lisp
@@ -1,7 +1,9 @@
 (in-package "EMOTIQ")
 
 ;;; Startup an Emotiq node
+
 (defun start-node ()
-  (gossip:gossip-startup :ping-others t))
-
-
+   (gossip:gossip-startup :ping-others nil)
+   ;(gossip:start-pinger-daemon) ; haven't written this yet but it will ping others periodically
+; must work with David to figure out what to do with the information it returns.
+  )
