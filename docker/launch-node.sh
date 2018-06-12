21c40d8df420c65ad9eb8420a7c57b701cbba754
Eugene Chupriyanov <tchu@tchu.ru>
2018-06-12 10:28:29 +0300

    Fixed typos and paths

--- docker/launch-node.sh
+++ docker/launch-node.sh
@@ -3,9 +3,9 @@
 docker run -d \
   -v $(pwd)/startup.sh:/startup.sh \
   -v $(pwd)/../src:/root/work/emotiq/src \
-  -v $(pwd)/../var/local/etc:/root/work/emotiq/var/local/etc \
+  -v $(pwd)/../var/etc:/root/work/emotiq/var/etc \
   -v $(pwd)/../etc:/root/work/emotiq/etc \
-  -v $HOME/emotiq.conf:/root/.config/common-lisp/source-registry.conf.d/emotiq.conf \
+  -v $(pwd)/emotiq.conf:/root/.config/common-lisp/source-registry.conf.d/emotiq.conf \
   -v $HOME/quicklisp:/root/quicklisp \
   -v $HOME/.ccl-init.lisp:/root/.ccl-quicklisp \
   -p 65002:65002 \
--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ccl -l /root/work/emotiq/etc/setup-emotiq-quicklisp.lisp </dev/null
+
 # Link library
-/root/work/emtotiq/etc/build-crypto-pairings.bash
+/root/work/emotiq/etc/build-crypto-pairings.bash
 ccl -e "(ql:quickload :emotiq/startup)" -e "(emotiq:main)"
