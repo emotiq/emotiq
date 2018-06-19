c2cc759841f7846c40d2d0bd6bc72aefae31ffe8
Mark Evenson <evenson.not.org@gmail.com>
2018-06-19 14:05:42 +0200

    Ensure that the REST/websocket servers bind to localhost tcp4

--- src/network/rest/server.lisp
+++ src/network/rest/server.lisp
@@ -1,18 +1,16 @@
 (in-package :emotiq-rest)
 
-(defvar *port* 3140)
-
-(defun start-server (&key (port *port*))
+(defun start-server (&key (port 3140) (host "127.0.0.1"))
   (let ((api :route.emotiq/0/0/1))
-    (let ((base-uri
-           (format nil "http://127.0.0.1:~a/" port))
-          (server 
-           (restas:start api
-                         :port port
-                         :acceptor-class 'restas:restas-acceptor)))
-      (emotiq:note "Starting REST server for api '~a' listening on port ~a" api port)
-      (emotiq:note "An index of available services be viewed as <~a>" base-uri)
-      server)))
+    (restas:start api
+                  :port port
+                  :acceptor-class 'restas:restas-acceptor
+                  :address host)
+    (emotiq:note "Starting REST server for api '~a' listening on port ~a" api port)
+    (with-slots ((port hunchentoot::port)
+                 (host hunchentoot::address))
+        (find port restas::*acceptors* :key #'hunchentoot:acceptor-port)
+      (emotiq:note "An index of available services be viewed as <http://~a:~a>" host port))))
             
 (defun stop-server ()
   (ignore-errors
