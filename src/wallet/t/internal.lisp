d26fba55bc6bd5a53bc593c4d8a8e4d6d203967b
Mark Evenson <evenson.not.org@gmail.com>
2018-06-19 14:05:41 +0200

    Make a wallet's secret synonymous with its 256 bit seed
    Test that that we get the same keys when we reconstruct a wallet from
    this seed.

--- src/wallet/t/internal.lisp
+++ src/wallet/t/internal.lisp
@@ -13,4 +13,21 @@
                      '(simple-array (unsigned-byte 8) (48))
                      "Derived AES256 keying is 48 unsigned bytes…"))))
 
+(prove:plan 2)
+(let* ((seed (ironclad:make-random-salt 32))
+       (wallet-1 (emotiq/wallet::make-wallet-from-seed seed))
+       (wallet-2 (emotiq/wallet::make-wallet-from-seed seed)))
+  (prove:is (hash:hash=
+             (hash:hash/256
+              (pbc:keying-triple-pkey (emotiq/wallet:keying-triple wallet-1)))
+             (hash:hash/256
+              (pbc:keying-triple-pkey (emotiq/wallet:keying-triple wallet-2))))
+            "Wallets from the same seed have the same public keys…")
+  (prove:ok (hash:hash=
+             (hash:hash/256
+              (pbc:keying-triple-skey (emotiq/wallet:keying-triple wallet-1)))
+             (hash:hash/256
+              (pbc:keying-triple-skey (emotiq/wallet:keying-triple wallet-2))))
+            "Wallets from the same seed have the same private keys…"))
+
 (prove:finalize)
