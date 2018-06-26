5c298d15f65e5ca63802aa63877ee956d7bc60b8
David McClain <dbm@emotiq.ch>
2018-06-26 08:44:34 -0700

    comment out section calling GOSSIP:ESTABLISH-GOSSIP-BROADCAST-GROUP - Gossip API not yet ready for prime time...

--- src/Cosi-BLS/cosi-handlers.lisp
+++ src/Cosi-BLS/cosi-handlers.lisp
@@ -894,10 +894,12 @@ check that each TXIN and TXOUT is mathematically sound."
   (or *cosi-gossip-neighborhood-graph*
       (setf *cosi-gossip-neighborhood-graph*
             (or :UBER ;; for now while debugging
+                #|
                 (gossip:establish-gossip-broadcast-group
                  (remove (node-pkey my-node) (get-witness-list)
                          :test 'int=)
-                 :graphID :cosi))
+                 :graphID :cosi)
+                |#)
             )))
 
 (defun gossip-neighborcast (my-node &rest msg)
