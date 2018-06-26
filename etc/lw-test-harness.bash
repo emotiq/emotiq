c1e3dc59d0a4160fdaed335645702d1bfeee6f12
Paul Tarvydas <paultarvydas@gmail.com>
2018-06-26 07:47:01 -0400

    wip

--- etc/lw-test-harness.bash
+++ etc/lw-test-harness.bash
@@ -26,7 +26,7 @@ DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # The command to run a Lisp test form:
 
-lisp="/Applications/LispWorks\ 7.1\ (64-bit)/LispWorks\ (64-bit).app/Contents/MacOS/lispworks-7-1-0-amd64-darwin"
+lisp="/Applications/LispWorks\ 7.1\ (64-bit)/LispWorks\ (64-bit).app/Contents/MacOS/lispworks-7-1-0-amd64-darwin"lisp="/Applications/LispWorks\ 7.1\ (64-bit)/LispWorks\ (64-bit).app/Contents/MacOS/lispworks-7-1-0-amd64-darwin"
 
 # The default whitespace list of systems to test
 systems=${systems:-":gossip-tests
