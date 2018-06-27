54c65a6a89864a76787abffd339e398bf0bb8165
David McClain <dbm@emotiq.ch>
2018-06-27 10:01:17 -0700

    add support for CALL-NEW-ELECTION

--- src/Cosi-BLS/cosi-construction.lisp
+++ src/Cosi-BLS/cosi-construction.lisp
@@ -113,6 +113,8 @@ THE SOFTWARE.
 
    (current-leader :accessor node-current-leader
                    :initform nil) ;; holds pkey of current leader node
+   (election-calls :accessor node-election-calls
+                   :initform 0)
    (tx-changes     :accessor node-tx-changes
                    :initform nil)
    ;; -------------------------------------
