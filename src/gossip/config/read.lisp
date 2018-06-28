671763c298afc400e46513405359935e8ef74c2a
Shannon Spires <ssos@bearlanding.com>
2018-06-28 09:45:32 -0600

    Add stakes.conf generation and reading.

--- src/gossip/config/read.lisp
+++ src/gossip/config/read.lisp
@@ -1,14 +1,15 @@
 (in-package :gossip/config)
 
-;;; XXX What's the need for these to be parameters? 
 (defparameter *keypairs-filename* "keypairs.conf")
-(defparameter *hosts-filename* "hosts.conf")
-(defparameter *machine-filename* "local-machine.conf")
+(defparameter *hosts-filename*    "hosts.conf")
+(defparameter *machine-filename*  "local-machine.conf")
+(defparameter *stakes-filename*   "stakes.conf")
 
 ;;; XXX No need for these to be parameters?
 (defparameter *keypair-db-path* nil "Full path to keypairs config file")
 (defparameter *hosts-db-path*   nil "Full path to hosts config file")
 (defparameter *machine-db-path* nil "Full path to local-machine config file")
+(defparameter *stakes-db-path*  nil "Full path to stakes config file")
 
 (defun find-root-path ()
   (merge-pathnames "gossip-config/" (emotiq/fs:etc/)))
@@ -16,8 +17,9 @@
 (defun initialize (&key (root-path (find-root-path)))
   (when root-path
     (setf *keypair-db-path* (merge-pathnames *keypairs-filename* root-path))
-    (setf *hosts-db-path*   (merge-pathnames *hosts-filename* root-path))
-    (setf *machine-db-path* (merge-pathnames *machine-filename* root-path))))
+    (setf *hosts-db-path*   (merge-pathnames *hosts-filename*    root-path))
+    (setf *machine-db-path* (merge-pathnames *machine-filename*  root-path))
+    (setf *stakes-db-path*  (merge-pathnames *stakes-filename*   root-path))))
 
 (defun get-values ()
   "Return the values for the current network configuration.
@@ -30,6 +32,7 @@ The third value contains the configuration of this node."
     (initialize))
   (handler-case
   (let ((keypairs (read-pairs-database *keypair-db-path*))
+        (stakes   (read-pairs-database *stakes-db-path*))
         (hosts nil)
         (local-machine nil))
     (flet ((badfile (filename)
@@ -39,7 +42,7 @@ The third value contains the configuration of this node."
           (unless hosts (gossip::log-event :WARN (format nil "'~A' invalid or empty" *hosts-filename*)))
       (setf local-machine (read-local-machine-configuration *machine-db-path*))
       (unless local-machine (badfile *machine-filename*))
-          (values keypairs hosts local-machine)))
+          (values keypairs hosts local-machine stakes)))
     (error (e)
            (emotiq:note "Configuration strategy failed because ~a" e)
            nil)))
@@ -58,7 +61,6 @@ The third value contains the configuration of this node."
     (with-open-file (s pathname :direction :input :if-does-not-exist :error)
       (let ((form (read s nil nil nil)))
         form))))
-
 
 
 
