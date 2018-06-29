1c78702e59d252a73d78883ce55c7c6670ca4023
David McClain <dbm@emotiq.ch>
2018-06-29 08:32:56 -0700

    set USE-REAL-GOSSIP true

--- src/Cosi-BLS/cosi-handlers.lisp
+++ src/Cosi-BLS/cosi-handlers.lisp
@@ -30,7 +30,7 @@ THE SOFTWARE.
 ;; ---------------------------------------------------------------
 
 (defvar *use-gossip*      t)     ;; use Gossip graphs instead of Cosi Trees
-(defvar *use-real-gossip* nil)   ;; set to T for real Gossip mode, NIL = simulation mode
+(defvar *use-real-gossip* t)     ;; set to T for real Gossip mode, NIL = simulation mode
 
 ;; ---------------------------------------------------------------
 
