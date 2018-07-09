7fa6dbb2b6161be6050ef3a3bf395598bcdbebfc
Shannon Spires <ssos@bearlanding.com>
2018-07-09 17:32:51 -0600

    Add *subpath* variable to filesystem.lisp to make it easier to test on multiple Lisp instances on one machine.

--- src/filesystem.lisp
+++ src/filesystem.lisp
@@ -11,36 +11,53 @@
 ;;;; time, we have to somehow "walk" their reference to copy them for
 ;;;; a "resource bundle" that can then be distributed.
 
+(defparameter *subpath* nil "Directory subpath to be added to each of the below. Must end with /. Useful for testing.")
+
 (defun var/log/ ()
   "Absolute cl:pathname of the directory to persist logs and traces of system activity."
   (let ((d (asdf:system-relative-pathname :emotiq "../var/log/")))
+    (when *subpath*
+      (setf d (merge-pathnames *subpath* d)))
     (ensure-directories-exist d)
     d))
 
 (defun etc/ ()
   "All configuration files for a node"
   (let ((d (asdf:system-relative-pathname :emotiq "../var/etc/")))
+    (when *subpath*
+      (setf d (merge-pathnames *subpath* d)))
     (ensure-directories-exist d)
     d))
 
 (defun tmp/ ()
   "Writable filesystem for temporary output"
-  (ensure-directories-exist
-   #p"/var/tmp/emotiq/"))
+  (let ((d  #p"/var/tmp/emotiq/"))
+    (when *subpath*
+      (setf d (merge-pathnames *subpath* d)))
+    (ensure-directories-exist d)))
   
 (defun emotiq/user/root/ ()
   #+:windows
   (error "Not implemented on Windows")
   #+:linux
-  (merge-pathnames ".emotiq/" (user-homedir-pathname))
+  (if *subpath*
+      (merge-pathnames *subpath* (merge-pathnames ".emotiq/" (user-homedir-pathname)))
+      (merge-pathnames ".emotiq/" (user-homedir-pathname)))
   #+:darwin
-  (merge-pathnames "Emotiq/"
-                   (merge-pathnames "Library/Application Support/"
-                                    (user-homedir-pathname))))
+  (if *subpath*
+      (merge-pathnames *subpath* (merge-pathnames "Emotiq/"
+                                                  (merge-pathnames "Library/Application Support/"
+                                                                   (user-homedir-pathname))))
+      (merge-pathnames "Emotiq/"
+                       (merge-pathnames "Library/Application Support/"
+                                        (user-homedir-pathname)))))
 
 (defun emotiq/wallet/ ()
   "The pathname for the directory containing wallets"
-  (merge-pathnames (make-pathname :directory '(:relative "wallet"))
-                   (emotiq/user/root/)))
+  (if *subpath*
+      (merge-pathnames *subpath* (merge-pathnames (make-pathname :directory '(:relative "wallet"))
+                                                  (emotiq/user/root/)))
+      (merge-pathnames (make-pathname :directory '(:relative "wallet"))
+                       (emotiq/user/root/))))
 
 
