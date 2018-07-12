77c19edbf902f93c54738bf461ef888d0c3575c3
Eugene Chupriyanov <tchu@tchu.ru>
2018-07-12 15:17:35 +0300

    USe linux libuv for linux (not osx version)

--- etc/install-libuv.bash
+++ etc/install-libuv.bash
@@ -9,7 +9,7 @@ EXTERNAL_LIBS_VERSION=release-0.1.13
 uname_s=$(uname -s)
 case ${uname_s} in
     Linux*)
-        LIBUV_URL=https://github.com/emotiq/emotiq-external-libs/releases/download/${EXTERNAL_LIBS_VERSION}/emotiq-libuv-osx.tgz
+        LIBUV_URL=https://github.com/emotiq/emotiq-external-libs/releases/download/${EXTERNAL_LIBS_VERSION}/emotiq-libuv-linux.tgz
         SUDO=sudo
         ;;
     Darwin*)
