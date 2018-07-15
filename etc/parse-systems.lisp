d81cde0448bb03f986d9b8b84092638628da46e9
Mark <evenson.not.org@gmail.com>
2018-07-15 15:28:18 +0200

    Better intergration of tests for both the CI and the developer
    CI now actually uses the contents of <file:etc/all-tests.sexp> to
    determine which tests should be run.
    
    Changes the mechanism used in test harness so that LispWorks can be
    invoked outside the CI.
    
    ci/lisp-wrapper-location.bash provides a function to interpolate a
    given name of Lisp implementation ("lispworks", "ccl" etc.) into an
    actual path to invoke.
    
    Invoking the test harness from the shell now reads the database of all
    defined tests in <file:etc/all-tests.sexp> rather than using a default
    value.

--- etc/parse-systems.lisp
+++ etc/parse-systems.lisp
@@ -1,10 +1,13 @@
 (ql:quickload :emotiq)
 (ql:quickload :alexandria)
 
+(in-package :cl-user)
 
-(let* ((all-tests.sexp
-        (asdf:system-relative-pathname :emotiq "../etc/all-tests.sexp"))
-       (s (alexandria:read-file-into-string all-tests.sexp))
-       (systems (read-from-string s)))
-  (format *standard-output* "~{~s ~}" systems))
+(defun parse-systems-into (file) 
+  (let* ((all-tests.sexp
+          (asdf:system-relative-pathname :emotiq "../etc/all-tests.sexp"))
+         (s (alexandria:read-file-into-string all-tests.sexp))
+         (systems (read-from-string s)))
+    (with-open-file (o file :direction :output :if-exists :supersede)
+      (format o "~{~s ~}" systems))))
 
