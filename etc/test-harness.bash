2bd291af508c3c4bee2a5261a88cda4c7e0caad1
Mark Evenson <evenson.not.org@gmail.com>
2018-07-15 11:09:50 +0200

    Speculative attempt to work with CI's use of lisp-harness.sh

--- etc/test-harness.bash
+++ etc/test-harness.bash
@@ -26,27 +26,32 @@ DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 lisp=${lisp:-'ccl'}
 
 function lisp_exec () {
-    tmpfile=$(mktemp /tmp/script-exec.lisp-XXXXXX)
-    while test $# -gt 0; do
-        case "$1" in
-            --eval|-e)
-                echo "$2" >> $tmpfile
-                shift
-                shift
-                ;;
-            --load|-l)
-                echo "(load \"$2\")" >> $tmpfile
-                shift
-                shift
-                ;;
-            *)
-                echo "Unknown argument $1!"
-                exit 1
-                ;;
-        esac
-    done
-    echo "(uiop:quit 0)" >> $tmpfile
-    ${lisp} <$tmpfile
+    # We're running in the CI
+    if [[ ${lisp} == "lisp-wrapper.sh" ]]; then
+        ${lisp} $*
+    else 
+        tmpfile=$(mktemp /tmp/script-exec.lisp-XXXXXX)
+        while test $# -gt 0; do
+            case "$1" in
+                --eval|-e)
+                    echo "$2" >> $tmpfile
+                    shift
+                    shift
+                    ;;
+                --load|-l)
+                    echo "(load \"$2\")" >> $tmpfile
+                    shift
+                    shift
+                    ;;
+                *)
+                    echo "Unknown argument $1!"
+                    exit 1
+                    ;;
+            esac
+        done
+        echo "(uiop:quit 0)" >> $tmpfile
+        ${lisp} <$tmpfile
+    fi
 }
 
 echo Test harness invoked using implementation: ${lisp}
@@ -54,7 +59,7 @@ echo $(lisp_exec --eval '(format t "~&~a ~a~&" (lisp-implementation-type)(lisp-i
 
 systems=${systems:-\
  $(systems_file=$(mktemp /tmp/systems-XXXXX); \
-   lisp_exec --load ${DIR}/parse-systems.lisp --eval "(parse-systems-into \"${systems_file}\")"; \
+   lisp_exec --load ${DIR}/parse-systems.lisp --eval "(parse-systems-into \"${systems_file}\")" >/dev/null; \
    cat < ${systems_file})}
 
 # Collect whitespace list of failed systems
