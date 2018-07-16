841afc5c8cf4bff7254fec565a490db568c8acc6
Karol Skocik <karol.skocik@gmail.com>
2018-07-16 13:59:37 +0200

    moved generation time functionality into separate package

--- src/test/emotiq-config.lisp
+++ src/test/emotiq-config.lisp
@@ -1,12 +1,12 @@
 (in-package :emotiq-config-test)
 
-;;;; INTERNAL testing 
+;;;; INTERNAL testing
 
 
 (define-test key-generation ()
-  (let* ((devops-plist emotiq/config:*dns-ip-zt.emotiq.ch*)
+  (let* ((devops-plist emotiq/config/generate:*dns-ip-zt.emotiq.ch*)
          (nodes-dns-ip devops-plist)
-         (nodes (emotiq/config::keys/generate nodes-dns-ip)))
+         (nodes (emotiq/config/generate:keys/generate nodes-dns-ip)))
     (assert-eq (length devops-plist)
                (length nodes))))
 
@@ -15,8 +15,8 @@
 
 
 (define-test network-generation ()
-   (let* ((devops-plist emotiq/config:*dns-ip-zt.emotiq.ch*)
-          (directories (emotiq/config:network/generate :nodes-dns-ip devops-plist)))
+   (let* ((devops-plist emotiq/config/generate:*dns-ip-zt.emotiq.ch*)
+          (directories (emotiq/config/generate:network/generate :nodes-dns-ip devops-plist)))
      (assert-eq (length devops-plist)
                 (length directories))))
 
@@ -24,11 +24,10 @@
 ;;;; FIXME:  this test will fail unless `cosi-bls` is loaded.  How to test?
 #+(or)
 (define-test get-stakes ()
-  (assert-true (emotiq/config:get-stakes)))           
+  (assert-true (emotiq/config:get-stakes)))
 
 ;;;; FIXME: Ensure that a configuration has been generated, then read
 ;;;; its genesis block, but do this without overwriting an existing configuration.
 #+(or)
 (define-test get-genesis-block ()
-  (assert-true (emotiq/config:get-genesis-block)))           
-             
+  (assert-true (emotiq/config:get-genesis-block)))
