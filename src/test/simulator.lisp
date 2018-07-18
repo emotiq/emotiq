bfb32d992aacaa99aec52a7f5c50e815b1706d71
David McClain <dbm@emotiq.ch>
2018-07-18 10:54:25 -0700

    temporary recode of emotiq-sim-test to track down the problem.

--- src/test/simulator.lisp
+++ src/test/simulator.lisp
@@ -1,9 +1,20 @@
 (in-package :emotiq-sim-test)
 
+(defun simulated-chain-fn ()
+  (and (emotiq/sim:initialize :nodes 3 :new-configuration-p t)
+       (emotiq/sim:run-new-tx)
+       (> (length (emotiq/sim:blocks))
+          1)))
+
+(define-test simulated-chain ()
+  (assert-true (simulated-chain-fn)))
+
+#|
 (define-test simulated-chain ()
   (assert-true (emotiq/sim:initialize :nodes 3 :new-configuration-p t))
   (assert-true (emotiq/sim:run-new-tx))
   (assert-true (> (length (emotiq/sim:blocks))
                   1)))
+|#
 
              
