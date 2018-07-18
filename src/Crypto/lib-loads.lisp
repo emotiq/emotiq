0ad5860547e2af18dae5c204cb1b93ef2ae19e0d
David McClain <dbm@emotiq.ch>
2018-07-18 02:07:29 -0700

    document raison d'etre for lib-loads.lisp

--- src/Crypto/lib-loads.lisp
+++ src/Crypto/lib-loads.lisp
@@ -1,3 +1,35 @@
+;; lib-loads.lisp -- Load C libraries for PBC and Edwards Curves
+;;
+;; DM/Emotiq 07/18
+;; ---------------------------------------------------------
+#|
+Copyright (c) 2018 Emotiq AG
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+|#
+;; --------------------------------------------------------------------
+;; The libs are defined and loaded here because CFFI expects them to be
+;; loaded and defined prior to macro-expansion of certain CFFI:DEFCFUN's.
+;;
+;; Our system is no longer in any danger by having these libs loaded
+;; along the way. Previously there were some MP issues that required a
+;; careful load and initialization order. That is no longer the case.
 
 (in-package :cl-user)
 
