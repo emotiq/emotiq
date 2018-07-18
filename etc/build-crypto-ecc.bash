0f17dc024077c3b6c5ee5a88ea1159465deabd82
Eugene Chupriyanov <tchu@tchu.ru>
2018-07-18 10:22:37 +0300

    FIxed building ECC libraries

--- etc/build-crypto-ecc.bash
+++ etc/build-crypto-ecc.bash
@@ -13,7 +13,7 @@
 # debug
 set -x
 
-EXTERNAL_LIBS_VERSION=release-0.1.8
+EXTERNAL_LIBS_VERSION=release-0.1.13
 
 DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -44,7 +44,7 @@ esac
 
 libs_url=https://github.com/emotiq/emotiq-external-libs/releases/download/${EXTERNAL_LIBS_VERSION}/emotiq-external-libs-${lib_suffix}.tgz
 
-mkdir -p ${var}/local
+mkdir -p ${var}/local/{lib,include}
 
 prefix=${var}/local
 lib=${prefix}/lib
