f023269f6c3db56d06f247bf6e9e9e834ad61942
Eugene Chupriyanov <tchu@tchu.ru>
2018-07-23 17:18:03 +0300

    Improvements to `demo-network/start-blockchain.bash`: * Stop script, if `(ql:quickload :emotiq/startup)` fails * Terminate running nodes, and start them again

--- demo-network/start-blockchain.bash
+++ demo-network/start-blockchain.bash
@@ -5,6 +5,15 @@ EMOTIQ_ROOT=$DIR/..
 
 # Pull in all dependencies
 $EMOTIQ_ROOT/ci/lisp-wrapper.bash -e '(ql:quickload :emotiq/startup)'
+status=$?
+
+if  [ $status -ne 0 ] ; then
+  echo "Failed to quickload Emotiq dependencies"
+  exit 1
+fi
+
+# Stop running node, if any
+${DIR}/stop-blockchain.bash > /dev/null 2>&1
 
 # Start the Emotiq blockchain
 for i in {1..3} ; do
