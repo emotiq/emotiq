2e1c58cee490d02f6e9a4d54ba8994a4b4877698
Eugene Chupriyanov <tchu@tchu.ru>
2018-07-27 12:45:47 +0300

    Increase LispWorks stack size to fix stack overflow in cffi

--- ci/install-lisp.bash
+++ ci/install-lisp.bash
@@ -51,6 +51,9 @@ cat >$rcfile <<EOF
       (load quicklisp-init)))
 EOF
 
+if [[ $LISP = lispworks* ]] ; then
+  echo '(setf system:*sg-default-size* 32000)' >> $rcfile
+
 $DIR/lisp-wrapper.bash -e '(format t "~&~A ~A up and running! (ASDF ~A)~2%"
                 (lisp-implementation-type)
                 (lisp-implementation-version)
