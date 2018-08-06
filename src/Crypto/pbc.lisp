812358d837777f3c1d62b861e6b3afe72bd15ca4
David McClain <dbm@emotiq.ch>
2018-08-06 14:25:57 -0700

    clean up make-keying-triple in pbc.lisp. Now directly works on public and secret keys. Should be incoming tagged items.

--- src/Crypto/pbc.lisp
+++ src/Crypto/pbc.lisp
@@ -2,12 +2,10 @@
 
 (defun make-keying-triple (public-key secret-key &optional signature)
   "Make a PBC:KEYING-TRIPLE from its integer representation of PUBLIC-KEY and SECRET-KEY"
-  (let* ((pkey (pbc:public-key public-key))
-         (skey (pbc:secret-key secret-key)))
-    (make-instance 'pbc:keying-triple
-                   :pkey pkey
-                   :skey skey
-                   :sig (or signature (pbc:sign-hash (hash:hash/256 pkey) skey)))))
+  (make-instance 'pbc:keying-triple
+                 :pkey public-key
+                 :skey secret-key
+                 :sig (or signature (pbc:sign-hash (hash:hash/256 public-key) skey))))
 
 (defun make-keying-pairs ()
   "Makes a public/private keypair seeded via UUID:MAKE-V1-UUID
